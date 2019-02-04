8900da0a85b64f86104b4d41427ae2047f16c902
ligiallucca <lilillucca@gmail.com>
2019-02-04 13:00:26 -0200

    versão do cypher para o segundo sprint

--- src/cipher.js
+++ src/cipher.js
@@ -1,87 +1,111 @@
 
-function getSentence() {
-    let sentenceInput = document.getElementById("sentenceOriginal");
-    return sentenceInput.value;
-}
+function getFormData(){
+  let form = document.getElementById("cypherForm");
 
-function getOffset() {
-    let offsetInput = document.getElementById("offset");
-    return parseInt(offsetInput.value);
+  let formData={
+    sentence: form.elements["sentence"].value,
+    offset: parseInt(form.elements["offset"].value),
+    decision: form.elements["decision"].value
+  }
 
+  return formData;
 }
 
 function getCharCodesArray(sentence) {
-    let charCodesArray = [];
-    for(let counter = 0; counter < sentence.length; counter++) {
-        charCodesArray.push(
-            sentence.charCodeAt(counter)
-        );
-    }
-    return charCodesArray;
-}
-
-function isCharLowercase (charCodeItem) {
-    let charCodea = 97;
-    let charCodez = 122;
-    if (charCodeItem >= charCodea && charCodeItem <= charCodez) {
-        return true;
-    }
-    return false;
+  let charCodesArray = [];
+  for(let index in sentence) {
+    charCodesArray.push(
+      sentence.charCodeAt(index)
+    );
+  }
+  return charCodesArray;
 }
 
 function isCharUppercase (charCodeItem) {
-    let charCodeA = 65;
-    let charCodeZ = 90;
-    if (charCodeItem >= charCodeA && charCodeItem <= charCodeZ) {
-        return true;
-    }
-    return false;
+  let charCodeA = 65;
+  let charCodeZ = 90;
+
+  if (charCodeItem >= charCodeA && charCodeItem <= charCodeZ) {
+      return true;
+  }
+  return false;
 }
 
-function applyOffset(charCodesArray, offset){
-    let offsetedArray = [];
-    let alphabetLength = 26;
-    
-    for(let counter = 0; counter < charCodesArray.length; counter++) {
-        let charCodeItem = charCodesArray[counter];
-        let offsetedItem;
-        let charCode = 97;
-        
-        if (isCharUppercase(charCodeItem)) {
-            charCode = 65;
-        }
-
-        if (isCharUppercase(charCodeItem) || isCharLowercase(charCodeItem)) {
-            offsetedItem = (charCodeItem - charCode + offset) % alphabetLength + charCode
-        } else {
-            offsetedItem = charCodeItem;
-        }
-
-        offsetedArray.push(offsetedItem);
-    }
-        return offsetedArray;
+function isCharLowercase (charCodeItem) {
+  let charCodeA = 97;
+  let charCodeZ = 122;
+  if (charCodeItem >= charCodeA && charCodeItem <= charCodeZ) {
+    return true;
+  }
+  return false;
 }
-   
+
+function encodeArray(sentence, offset) {
+  let charCodesArray = getCharCodesArray(sentence);
+  let encodedArray = [];
+
+  for (let code of charCodesArray) {
+    if (isCharUppercase(code)) {
+      encodedArray.push((((code - 65) + offset) % 26) + 65);
+    } else if (isCharLowercase(code)) {
+      encodedArray.push((((code - 97) + offset) % 26) + 97);
+    } else {
+      encodedArray.push(code);
+    }
+  }
+
+  return encodedArray;
+}
+
+function decodeArray(sentence, offset) {
+  let charCodesArray = getCharCodesArray(sentence);
+  let decodedArray = [];
+
+  for (let code of charCodesArray) {
+    if (isCharUppercase(code)) {
+      decodedArray.push((((code - 90) - offset) % 26) + 90);
+    } else if (isCharLowercase(code)) {
+      decodedArray.push((((code - 122) - offset) % 26) + 122);
+    } else {
+      decodedArray.push(code);
+    }
+  }
+  return decodedArray;
+}
+
+function getCypher(decision, sentence, offset) {
+  let cypher = [];
+
+  if (decision === "encode") {
+    cypher = encodeArray(sentence, offset);
+  } else {
+    cypher = decodeArray(sentence, offset);
+  }
+
+  return cypher;
+}
+
 function setStringFromArray(array){
-    let result = ""
-    for (let counter=0; counter <array.length; counter++)
+  let result = "";
+
+  for (let counter=0; counter <array.length; counter++) {
     result = result + String.fromCharCode(array[counter]);
-    return result;     
-   }
+  }
 
-function encode(sentence,offset) {
- 
-    let CodesArray = getCharCodesArray(sentence);
-    let withOffset = applyOffset(CodesArray,offset);
-
-return setStringFromArray(withOffset);
-  
+  return result;
 }
 
-function result (){
-let sentence = getSentence();
-let offset = getOffset();
-document.getElementById("mostraCifra").innerHTML = encode(sentence, offset);
+function result(){
+  let form = getFormData();
+  let cypher = getCypher(form.decision, form.sentence, form.offset);
+  let result = setStringFromArray(cypher);
+
+  return result;
 }
 
+function showResult() {
 
+  document.getElementById("result").innerHTML=result();
+
+
+}
--- src/tests.js
+++ src/tests.js
@@ -1,6 +1,21 @@
-fuction verifyEncode{
-if (encode("efg",3)===hij){
-    console.log("certo");
-}
-else console.log("xi");
-}
+function compareObjects ( decision, sentence, offset, obj1, obj2) {
+    if (setStringFromArray(obj1) === setStringFromArray(obj2)) {
+        console.log(
+            decision + ' "' + sentence + '" com o deslocamento ' + offset +  ' deu o resultado: certo'
+        );
+    }
+    else{
+        console.log(
+            decision + ' "' + sentence + '" com o deslocamento ' + offset +  ' deu o resultado: errado'
+        );
+    }
+};
+
+compareObjects("encode", "abc", 1, encodeArray("abc",1), [98,99,100]);
+compareObjects("decode", "bcd", 1, decodeArray("bcd",1), [97,98,99]);
+compareObjects("encode", "ABC", 2,encodeArray("ABC",2), [67,68,69]);
+compareObjects("decode", "CDE", 2,decodeArray("CDE",2), [65,66,67]);
+compareObjects("encode", "AbC", 100,encodeArray("AbC",100), [87,120,89]);;
+compareObjects("decode", "BcD", 1,decodeArray("BcD",1), [65,98,67]);
+compareObjects("decode", "DeF", 100,decodeArray("DeF",100), [72,105,74]);
+
